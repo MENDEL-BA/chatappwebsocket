2f5c72537d9d567c5b1993f841611d72c54d2cf0
BA Papa <papa.mendel@ext.soprasteria.com>
2024-03-31 23:32:24 +0200

    Initialisation project

---
+++ src/test/java/com/yourway/car/CarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourway.car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
